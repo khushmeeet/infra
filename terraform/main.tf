cbcf1a8ffbad015ed72f1a6b80152888633307b9
Khushmeet Singh <khushmeetsingh1996@gmail.com>
2023-05-15 02:11:56 -0700

    Created TF script for provisioning

---
+++ terraform/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "~1.38.2"
+    }
+  }
+}
+
+provider "hcloud" {
+  token = var.hcloud_token
+}
+
+resource "hcloud_ssh_key" "axion-ssh" {
+  name       = "Axion SSH Key"
+  public_key = file("~/.ssh/id_ed25519.pub")
+}
+
+resource "hcloud_server" "axion" {
+  name              = var.server_name
+  location          = var.server_location
+  image             = var.server_image
+  server_type       = var.server_type
+  backups           = true
+  delete_protection = false
+  ssh_keys          = hcloud_ssh_key.axion-ssh.id
+  user_data         = file("cloud-init/cloud-init.yaml")
+
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = true
+  }
+
+  labels = {
+    name = var.server_name
+  }
+}
+
+# TODO: Volume backup
+
+resource "hcloud_volume" "axion-root" {
+  name              = var.volume_name
+  size              = var.volume_size
+  location          = var.volume_location
+  server_id         = hcloud_server.axion.id
+  automount         = true
+  format            = var.volume_format
+  delete_protection = false
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,44 @@
+variable "hcloud_token" {
+  description = "Value of hcloud_token to connect to Hetzner Cloud"
+  sensitive   = true
+}
+
+variable "server_name" {
+  description = "Name of the VM to connect"
+  default     = "axion-1"
+}
+
+variable "server_location" {
+  description = "Location of the VM in Hetzner Cloud"
+  default     = "nbg1"
+}
+
+variable "server_image" {
+  description = "Image ID for the VM"
+  default     = "ubuntu-22.04"
+}
+
+variable "server_type" {
+  description = "Type of instance based on CPU and RAM"
+  default     = "cx21"
+}
+
+variable "volume_name" {
+  description = "Name of the volume in  Hetzner Cloud"
+  default     = "axion-root"
+}
+
+variable "volume_size" {
+  description = "Size of the volume in  GiB"
+  default     = 50
+}
+
+variable "volume_location" {
+  description = "Location of the volume in  Hetzner Cloud"
+  default     = "nbg1"
+}
+
+variable "volume_format" {
+  description = "File system format of the volume"
+  default     = "ext4"
+}
