84c56f1878762701f05d82d7089112d0066ea7b5
Khushmeet Singh <khushmeetsingh1996@gmail.com>
2023-05-28 08:09:53 -0700

    Created docker service for pg backup

---
+++ pg_backup/pg_backup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+current_year=$(date +%Y)
+current_month=$(date +%m)
+current_date=$(date +%d)
+
+# Set backup directory and filename
+BACKUP_DIR="${HOME}/backups/"
+BACKUP_FILENAME="pg_backup_${current_date}.sql"
+S3_LOCATION="s3://infra-pg-backup/${current_year}/${current_month}/
+
+# Create backup
+PGPASSWORD=$POSTGRES_PASSWORD postgres pg_dumpall -h $POSTGRES_HOST -U $POSTGRES_USER -Fc > $BACKUP_DIR/$BACKUP_FILENAME
+
+gzip $BACKUP_DIR/$BACKUP_FILENAME
+echo "[$(date +'%Y-%m-%d %H:%M:%S')]Backup complete: $BACKUP_DIR/$BACKUP_FILENAME.gz"
+
+aws s3 cp $BACKUP_DIR/$BACKUP_FILENAME.gz S3_LOCATION
+echo "[$(date +'%Y-%m-%d %H:%M:%S')]Copy to S3 complete: $BACKUP_DIR/$BACKUP_FILENAME.gz"
