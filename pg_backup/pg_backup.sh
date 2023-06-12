ec7409857f63f94882b93e523b3822476698a6a7
Khushmeet Singh <khushmeetsingh1996@gmail.com>
2023-06-12 02:14:52 -0700

    Added installation for postgres15

--- pg_backup/pg_backup.sh
+++ pg_backup/pg_backup.sh
@@ -5,9 +5,11 @@ current_month=$(date +%m)
 current_date=$(date +%d)
 
 # Set backup directory and filename
-BACKUP_DIR="${HOME}/backups/"
+BACKUP_DIR="${HOME}/backups"
 BACKUP_FILENAME="pg_backup_${current_date}.sql"
 S3_LOCATION="s3://infra-pg-backup/${current_year}/${current_month}/"
+
+mkdir -p "$BACKUP_DIR"
 
 # Create backup
 PGPASSWORD="$POSTGRES_PASSWORD" postgres pg_dumpall -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -Fc > "$BACKUP_DIR/$BACKUP_FILENAME"
