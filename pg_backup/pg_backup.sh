b3bcd90a9331229eb1640204960b9e4a315cd513
Khushmeet Singh <khushmeetsingh1996@gmail.com>
2023-06-12 22:46:36 -0700

    Bug fixing

--- pg_backup/pg_backup.sh
+++ pg_backup/pg_backup.sh
@@ -12,10 +12,12 @@ S3_LOCATION="s3://infra-pg-backup/${current_year}/${current_month}/"
 mkdir -p "$BACKUP_DIR"
 
 # Create backup
-PGPASSWORD="$POSTGRES_PASSWORD" postgres pg_dumpall -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -Fc > "$BACKUP_DIR/$BACKUP_FILENAME"
+PGPASSWORD="$POSTGRES_PASSWORD" pg_dumpall -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -Fc > "$BACKUP_DIR/$BACKUP_FILENAME"
 
 gzip "$BACKUP_DIR/$BACKUP_FILENAME"
 echo "[$(date +'%Y-%m-%d %H:%M:%S')]Backup complete: $BACKUP_DIR/$BACKUP_FILENAME.gz"
 
-aws s3 cp "$BACKUP_DIR/$BACKUP_FILENAME.gz" S3_LOCATION
+aws s3 cp "$BACKUP_DIR/$BACKUP_FILENAME.gz" $S3_LOCATION
 echo "[$(date +'%Y-%m-%d %H:%M:%S')]Copy to S3 complete: $BACKUP_DIR/$BACKUP_FILENAME.gz"
+
+rm -rf $BACKUP_DIR/*
